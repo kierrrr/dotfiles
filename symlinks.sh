b324ccc3bf06d44661ee690fbbd11bfebf73c195
Kier Lomugdang <klomugdang@atlassian.com>
2025-05-27 21:57:43 +1000

    Added symlinks

---
+++ symlinks.sh
@@ -0,0 +1,7 @@
+ln -sv ~/dotfiles/.zshrc ~/.zshrc -f
+ln -sv ~/dotfiles/.config/lazygit ~/.config/lazygit -f
+ln -sv ~/dotfiles/.config/nvim/lazyvim.json ~/.config/nvim/lazyvim.json -f
+ln -sv ~/dotfiles/.config/scripts ~/.config/scripts -f
+ln -sv ~/dotfiles/.config/nvim/lua/config ~/.config/nvim/lua/config -f
+ln -sv ~/dotfiles/.config/nvim/lua/plugins ~/.config/nvim/lua/plugins -f
+ln -sv ~/dotfiles/.config/tmux/tmux.conf ~/.config/tmux/tmux.conf -f
