8d7132c47e10fd3b9a6680e7545c64a1dd8ca88e
Kier Lomugdang <klomugdang@atlassian.com>
2025-05-27 22:03:11 +1000

    Update brew

--- brew.sh
+++ brew.sh
@@ -1,1 +1,16 @@
-brew install fzf zsh-syntax-highlighting fd node ripgrep tmux neovim-remote delta zoxide joshmedeski/sesh/sesh gnu-sed luarocks imagemagick bottom lazygit
+brew install fzf \
+  zsh-syntax-highlighting \
+  fd \
+  node \
+  ripgrep \
+  tmux \
+  neovim-remote \
+  delta \
+  zoxide \
+  joshmedeski/sesh/sesh \
+  gnu-sed \
+  luarocks \
+  imagemagick \
+  bottom \
+  lazygit \
+  nvm
